8922b4da1df6aa3d6a712c5025ac68f52da2825c
yoga <prasetyo.yoga1109@gmail.com>
2023-02-03 08:46:25 +0700

    Fixing serverless function timed out on vercel prod

--- pages/anime/[id].tsx
+++ pages/anime/[id].tsx
@@ -1,48 +1,10 @@
 import MainLayout from '@/components/MainLayout';
 import Detail from '@/section/anime-detail/Detail';
 
-import { animeDetailQuery } from '@/lib/queries/anime';
-
-import type { GetServerSidePropsContext } from 'next';
-import type { AnimeDetailProps } from '@/models/anime';
-
-interface PageProps {
-  detail: AnimeDetailProps;
-}
-
-export async function getServerSideProps({ params }: GetServerSidePropsContext) {
-  const variables = {
-    id: Number(params?.id),
-  };
-
-  const url = 'https://graphql.anilist.co';
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-    },
-    body: JSON.stringify({
-      query: animeDetailQuery,
-      variables,
-    }),
-  };
-
-  const request = await fetch(url, options);
-  const result = await request.json();
-
-  if (!result) return { props: {} }
-  return {
-    props: {
-      detail: result.data.Media,
-    }
-  };
-};
-
-const AnimeDetail = ({ detail }: PageProps) => {
+const AnimeDetail = () => {
   return (
     <MainLayout>
-      <Detail {...detail} />
+      <Detail />
     </MainLayout>
   )
 };
--- section/anime-detail/Detail.tsx
+++ section/anime-detail/Detail.tsx
@@ -1,41 +1,68 @@
 import { useEffect, useState } from 'react';
-import { Box, Button, Grid, Typography } from '@mui/material';
+import { useRouter } from 'next/router';
+import { Box, Button, Typography } from '@mui/material';
 import { Star } from '@mui/icons-material';
 import { Favorite, FavoriteBorder } from '@mui/icons-material';
-
 import { FavoriteAnime } from '@/lib/storage/favoriteAnime';
+import { animeDetailQuery } from '@/lib/queries/anime';
 
 import type { AnimeDetailProps } from '@/models/anime';
 
-const List = ({
-  id,
-  bannerImage,
-  coverImage,
-  title,
-  startDate,
-  episodes,
-  duration,
-  averageScore,
-  genres,
-  description,
-}: AnimeDetailProps) => {
+const List = () => {
+  const router = useRouter();
+  const [anime, setAnime] = useState<AnimeDetailProps | null>(null);
+  const fetchAPI = async () => {
+    const variables = {
+      id: router.query.id,
+    };
+  
+    const url = 'https://graphql.anilist.co';
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({
+        query: animeDetailQuery,
+        variables,
+      }),
+    };
+  
+    const request = await fetch(url, options);
+    const result = await request.json();
+
+    if (result) {
+      setAnime(result.data.Media);
+    }
+  };
+
   let episodeString = 'Still ongoing';
-  if (episodes === 1) episodeString = `${duration} min`;
-  else if (episodes !== null) episodeString = `${episodes} episodes`;
+  if (anime?.episodes === 1) episodeString = `${anime?.duration} min`;
+  else if (anime?.episodes !== null) episodeString = `${anime?.episodes} episodes`;
 
   const [isAddedToFavorite, setIsAddedToFavorite] = useState(false);
 
   const addToFavorite = () => {
     const favoriteAnime = new FavoriteAnime();
-    if (!favoriteAnime.get().includes(Number(id))) {
-      favoriteAnime.save(Number(id));
+    // if (!favoriteAnime.get().includes(Number(id))) {
+    //   favoriteAnime.save(Number(id));
+    //   setIsAddedToFavorite(true);
+    // }
+    if (!favoriteAnime.get().includes(Number(anime?.id))) {
+      favoriteAnime.save(Number(anime?.id));
       setIsAddedToFavorite(true);
     }
   };
 
   useEffect(() => {
     const favoriteAnime = new FavoriteAnime();
-    if (favoriteAnime.get().includes(Number(id))) setIsAddedToFavorite(true);
+    if (favoriteAnime.get().includes(Number(anime?.id))) setIsAddedToFavorite(true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [anime]);
+
+  useEffect(() => {
+    fetchAPI();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -45,7 +72,7 @@ const List = ({
       <Box position='relative' width='100%' height='250px'>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          src={bannerImage}
+          src={anime?.bannerImage}
           alt='banner'
           style={{ width: '100%', height: '100%', objectFit: 'cover' }}
         />
@@ -56,25 +83,25 @@ const List = ({
         <Box position='relative' width='100px' height='160px'>
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
-            src={coverImage.large}
+            src={anime?.coverImage.large}
             alt='cover'
             style={{ width: '100%', height: '100%', objectFit: 'cover' }}
           />
         </Box>
         <Box marginLeft='1rem'>
-          <Typography variant='h5'>{title.userPreferred}</Typography>
-          <Typography variant='body1'>{startDate.year}</Typography>
+          <Typography variant='h5'>{anime?.title.userPreferred}</Typography>
+          <Typography variant='body1'>{anime?.startDate.year}</Typography>
           <Typography variant='body1'>
             {episodeString}
           </Typography>
           <Box display='flex' alignItems='center'>
             <Star sx={{ color: 'yellow', marginRight: '8px' }} />
             <Typography variant='body1'>
-              {averageScore}/100
+              {anime?.averageScore}/100
             </Typography>
           </Box>
           <Typography variant='body1'>
-            Genre: {JSON.stringify(genres).replace(/\"|\[|\]/g, '').replace(/\,/g, ', ')}
+            Genre: {JSON.stringify(anime?.genres)?.replace(/\"|\[|\]/g, '').replace(/\,/g, ', ')}
           </Typography>
           <Button onClick={addToFavorite} sx={{ color: 'white' }}>
             {isAddedToFavorite
@@ -91,7 +118,7 @@ const List = ({
         <Typography variant='h6'>
           Description
         </Typography>
-        <div dangerouslySetInnerHTML={{ __html: description }} />
+        <div dangerouslySetInnerHTML={{ __html: anime?.description ?? '' }} />
       </Box>
     </Box>
   );
